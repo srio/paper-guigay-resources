a9d8ae6bd7f044b596bd3e88e63299062276f3ac
Manuel Sanchez del Rio <srio@esrf.eu>
2025-06-27 17:07:58 +0200

    working

--- scripts_new/paper2016bis.py
+++ scripts_new/paper2016bis.py
@@ -18,7 +18,7 @@ from srxraylib.plot.gol import plot, set_qt, plot_show
 #
 # Equation 24 in Guigay and Ferrero (2016)
 #
-def integrate_sgplus(x, q, k=0.0, a=0.0, mu1=0.0, R=0.0, beta=0.0, acmax=0.0, kiny=0.0,
+def integrate_sgplus(x, q, k=0.0, a=0.0, mu1=0.0, R=0.0, beta=0.0, acmax=0.0, omega_imag=0.0,
                     gamma=0, teta=0, Aeq17=0, # NEWWWWWWWWWWWWWWW
                      ):
     v = numpy.linspace(0, a, 1000)
@@ -27,14 +27,35 @@ def integrate_sgplus(x, q, k=0.0, a=0.0, mu1=0.0, R=0.0, beta=0.0, acmax=0.0, ki
     invle = 1 / q - mu1 / R
 
     for i in range(v.size):
-        # y is defined before eq 23: y = A gamma (a^2-u^2) / sin^2(2theta)
-        y = Aeq17 *  gamma * (a**2-v[i]**2) / (numpy.sin(2*teta))**2
-        # y0 = acmax * (1 - (v[i] / a)**2)
-        # print("y: ", y0, y)
+        # y is defined just before eq 23
+        y = Aeq17 *  gamma * (a**2 - v[i]**2) / (numpy.sin(2 * teta))**2
+        # y =  acmax * (1 - (v[i] / a)**2)
+        # print(">>>> y: ", y0, y, Aeq17, gamma, a, teta)
 
         yy[i] = mpmath.hyp1f1(1j * beta, 1, 1j * y) * \
-            numpy.exp(1j * k * 0.5 * v[i]**2 * invle) * numpy.cos(k * v[i] * (x / q - 1j * kiny))
+            numpy.exp(1j * k * 0.5 * v[i]**2 * invle) * numpy.cos(k * v[i] * (x / q - 1j * omega_imag))
     return 2 * numpy.trapz(yy, x=v)
+
+def integrate_sgplus_notcentered(x, q, k=0.0, a=0.0, mu1=0.0, R=0.0, beta=0.0, acmax=0.0, omega_imag=0.0,
+                    gamma=0, teta=0, Aeq17=0, xc_over_q=0, # NEWWWWWWWWWWWWWWW
+                     ):
+    v = numpy.linspace(-a, a, 2000)
+    yy = numpy.zeros_like(v, dtype=complex)
+
+    invle = 1 / q - mu1 / R
+
+    for i in range(v.size):
+        # y is defined just before eq 23
+        y = Aeq17 *  gamma * (a**2 - v[i]**2) / (numpy.sin(2 * teta))**2
+        # y =  acmax * (1 - (v[i] / a)**2)
+        # print(">>>> y: ", y0, y, Aeq17, gamma, a, teta)
+
+        # print(">>>>", xc_over_q * q, numpy.exp(k * v[i] * (-1j * (x - xc_over_q * q) / q - omega_imag)))
+        yy[i] = mpmath.hyp1f1(1j * beta, 1, 1j * y) * \
+            numpy.exp(1j * k * 0.5 * v[i]**2 * invle) * \
+            numpy.exp(k * v[i] * (-1j * (x - xc_over_q * q) / q - omega_imag))
+    return numpy.trapz(yy, x=v)
+
 
 def get_max(xx, yy, verbose=1):
     i = numpy.argmax(yy)
@@ -62,9 +83,9 @@ def run_asymmetric(plot_qscan=1, plot_xscan=1):
     k = 2 * numpy.pi / lambda1
     h = 2 * k * numpy.sin(teta)
 
-    chimh = -1j * chih;
+    chimh = -1j * chih
     chih2 = chih * chimh
-    u2 = 0.25 * chih2 * k**2
+    Omega = 0.25 * chih2 * k**2  #  this is Omega (see just after eq 7b)
     thickness = 1
     p = 0
     R = 2000
@@ -90,12 +111,17 @@ def run_asymmetric(plot_qscan=1, plot_xscan=1):
     # qpoly = p * R * gam2 / (2 * p + R * gam1)
     att = 1.0 # numpy.exp(-k * 0.5 * (t1 + t2) * numpy.imag(chizero))
     s2max = 0.25 * t1 * t2
-    u2max = u2 * s2max  # Omega = k**2 chi_h chi_hbar / 4 ? (end of pag 490)
+    u2max = Omega * s2max
     gamma = t2 / t1
-    a = numpy.sin(2 * teta) * t1 * 0.5
-    kin = 0.25 * (t1 - t2) * chizero / a
-    kinx = numpy.real(kin)
-    kiny = numpy.imag(kin)
+    # a is defined after eq 22:
+    # a = numpy.sin(2 * teta) * t1 * 0.5
+    a = (t1 + t2) / 2 * numpy.sin(2*teta) / (1+gamma)
+
+    omega = 0.25 * (t1 - t2) * chizero / a # omega following the definition found after eq 22
+
+    print(">>>> omega=", omega, chizero * (1 - gamma) / 2 / numpy.sin(2 * teta))
+    omega_real = numpy.real(omega)
+    omega_imag = numpy.imag(omega)
     # com = numpy.sin(alfa) * (1 + gam1 * gam2 * (1 + poisson_ratio))
     kp3 = 0.5 * k * (gamma * a)**2
     mu1 = (numpy.cos(alfa) * 2 * fam1 * gam1 + numpy.sin(alfa) * (fam1**2 + poisson_ratio * gam1**2)) / (numpy.sin(2*teta)* numpy.cos(teta))
@@ -104,6 +130,8 @@ def run_asymmetric(plot_qscan=1, plot_xscan=1):
     a2 = (0.5 * thickness / numpy.cos(teta)) * (numpy.cos(alfa) * numpy.sin(teta2) + poisson_ratio*numpy.sin(alfa) * numpy.cos(teta2))
     acrist = -h * numpy.sin(alfa) * (1 + gam1 * gam2 * (1 + poisson_ratio)) / R # A in eq 17
     Aeq17 = acrist
+
+    xc_over_q = omega_real - t1 * numpy.sin(alfa + teta) / (2 * R)
 
     # y is defined before eq 23: y = A gamma (a^2-u^2) / sin^2(2theta)
     # y = Aeq17 * (1 - (v[i] / a) ** 2)
@@ -121,7 +149,7 @@ def run_asymmetric(plot_qscan=1, plot_xscan=1):
         for j in range(qq.size):
             amplitude = integrate_sgplus(0, qq[j],
                                    k=k, a=a, mu1=mu1, R=R,
-                                   beta=beta, acmax=acmax, kiny=kiny,
+                                   beta=beta, acmax=acmax, omega_imag=omega_imag,
                                    gamma=gamma, teta=teta, Aeq17=Aeq17, # NEWWWWWWWWWW
                                    ) / numpy.sqrt(numpy.abs(lambda1 * qq[j]))
             yy[j] = att * numpy.abs(amplitude)**2
@@ -133,20 +161,36 @@ def run_asymmetric(plot_qscan=1, plot_xscan=1):
     else:
         qposition = 1680.96
 
-    # x-scan
+    # x-scan centered
     if plot_xscan:
-        print("Calculating x-scan...")
-        # xx = numpy.linspace(-0.0025, .0025, 200)
-        xx = numpy.linspace(-a, a, 1000)
+        print("Calculating x-scan (centered)...")
+        xx = numpy.linspace(-0.0025, .0025, 200)
+        # xx = numpy.linspace(-a, a, 1000)
         yy = numpy.zeros_like(xx)
         for j in range(xx.size):
             amplitude = integrate_sgplus(xx[j], qposition,
                                    k=k, a=a, mu1=mu1, R=R,
-                                   beta=beta, acmax=acmax, kiny=kiny,
+                                   beta=beta, acmax=acmax, omega_imag=omega_imag,
+                                   gamma=gamma, teta=teta, Aeq17=Aeq17,  # NEWWWWWWWWWW
                                    ) / numpy.sqrt(numpy.abs(lambda1 * qposition))
             yy[j] = att * numpy.abs(amplitude)**2
         plot(xx, yy,
-             xtitle='xi [mm]', ytitle="Intensity on axis", title="alfa=%g deg" % (alfa_deg),
+             xtitle='x - xc[mm]', ytitle="Intensity on axis", title="alfa=%g deg" % (alfa_deg),
+             show=0)
+
+        print("Calculating x-scan (not centered)...")
+        # xx = numpy.linspace(-0.0025, .0025, 200)
+        xx = numpy.linspace(-a, a, 1000)
+        yy = numpy.zeros_like(xx)
+        for j in range(xx.size):
+            amplitude = integrate_sgplus_notcentered(xx[j], qposition,
+                                   k=k, a=a, mu1=mu1, R=R,
+                                   beta=beta, acmax=acmax, omega_imag=omega_imag,
+                                   gamma=gamma, teta=teta, Aeq17=Aeq17, xc_over_q=xc_over_q # NEWWWWWWWWWW
+                                   ) / numpy.sqrt(numpy.abs(lambda1 * qposition))
+            yy[j] = att * numpy.abs(amplitude)**2
+        plot(xx, yy,
+             xtitle='x [mm] (NOT CENTERED)', ytitle="Intensity on axis", title="alfa=%g deg" % (alfa_deg),
              show=0)
 
 if __name__ == "__main__":
@@ -159,6 +203,6 @@ if __name__ == "__main__":
     #
     # asymmetric Fig. 2
     #
-    run_asymmetric(plot_qscan=1, plot_xscan=0)
+    run_asymmetric(plot_qscan=0, plot_xscan=1)
 
     plot_show()
